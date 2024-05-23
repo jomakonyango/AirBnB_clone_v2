1617ea8e354bc9c666e15da4a7721098f6d34d3e
John Otieno <jomakonyango@gmail.com>
2024-05-23 06:17:43 +0300

    code update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+class FileStorage:
+    ...
+
+    def close(self):
+        """Deserializes the JSON file to objects"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -6,3 +6,17 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+class State(BaseModel, Base):
+    ...
+    if HBNB_TYPE_STORAGE != 'db':
+        @property
+        def cities(self):
+            """Returns the list of City instances with state_id equals to the current State.id"""
+            from models import storage
+            from models.city import City
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
+
